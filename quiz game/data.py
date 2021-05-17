0b08e87b1a5081bff53510d27a38a7a7488b00e8
Nicolae <67197181+Nicolae77@users.noreply.github.com>
2021-05-17 13:21:04 +0100

    Add files via upload

---
+++ quiz game/data.py
@@ -0,0 +1,13 @@
+question_data = [
+    {"text": "Boris Johnson is a Italian politician ", "answer": "False"},
+    {"text": "Python is the most popular programming language ", "answer": "True"},
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal in Africa Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human are in the feet.", "answer": "True"},
+    {"text": "The total surface are of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was original called 'Backrub'.", "answer": "True"},
+    {"text": "The larges country in the world by territory is Russia.", "answer": "True"},
+    {"text": "The capital of Germany is Madrid", "answer": "False"},
+]
